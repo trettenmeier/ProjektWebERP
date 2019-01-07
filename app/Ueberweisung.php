7c87be6c1249b653fae97d6b92ade35fe8d1e14b
T. Rettenmeier <46442407+trettenmeier@users.noreply.github.com>
2019-01-07 16:51:34 +0100

    Update Ueberweisung.php

--- app/Ueberweisung.php
+++ app/Ueberweisung.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\Input;
 
 class Ueberweisung extends Model
 {
-    //ist so konfiguriert, dass es nur unter LINUX funktioniert!!!!!!!!
     public function doUebereweisung()
     {
         $meinKonto = DB::table('einrichtung')->first();
@@ -19,7 +18,7 @@ class Ueberweisung extends Model
 
         unset($input["_token"]);    //token muss weg, damit fehlerfrei gelooped wird
         foreach ($input as $item) {
-            //hier noch: nur berücksichtigen, wenn auch ausgewählt
+            //hier noch: nur berÃ¼cksichtigen, wenn auch ausgewÃ¤hlt
             if (!isset($item['erstellen'])) {
                continue;
             }
@@ -34,9 +33,9 @@ class Ueberweisung extends Model
             //ansonsten: automatische berechnung aus alter + awg-wiGeld
             else {
                 if (isset($item['aussenwohnung']) && $item['aussenwohnung'] == "on") {
-                    $betrag = $betrag + 90; //180 euro WiGeld für AWG-Bewohner pro Monat
+                    $betrag = $betrag + 90; //180 euro WiGeld fÃ¼r AWG-Bewohner pro Monat
                 }
-                //taschengeldsatz für monat dazuzählen
+                //taschengeldsatz fÃ¼r monat dazuzÃ¤hlen
                 $betrag = $betrag + $item['taschengeld']/2;
             }
 
@@ -49,10 +48,10 @@ class Ueberweisung extends Model
             $name = $item['name'] . ", " . $item['vorname'];
 
             $data = [
-                'Zahlungsempfänger' => $name,
+                'ZahlungsempfÃ¤nger' => $name,
                 'Angaben zum Kontoinhaber' => $meinKonto->konto_bezeichnung,
                 'Datum' => $datum->format('d.m.Y'),
-                'Verwendungszweck' => $item['betreff'], //hier noch länge kontrollieren
+                'Verwendungszweck' => $item['betreff'], //hier noch lÃ¤nge kontrollieren
                 'noch Verwendungszweck' => '',
                 'Betrag Euro Cent' => $betrag,
                 'Kreditinstitut' => $meinKonto->konto_bezeichnung,
@@ -68,11 +67,11 @@ class Ueberweisung extends Model
             $pdf->flatten()->save(public_path() . "/modul_ueberweisung/temp/output" . $i++ . ".pdf");
         }
 
-        // alles zusammenfügen
+        // alles zusammenfÃ¼gen
         $command = "pdftk \"" . public_path() . "/modul_ueberweisung/temp/*.pdf\" cat output \"" . public_path() . "/modul_ueberweisung/output.pdf\"";
         exec($command);
 
-        // das temp-verzeichnis danach wieder aufräumen
+        // das temp-verzeichnis danach wieder aufrÃ¤umen
         $fullPath = public_path() . "/modul_ueberweisung/temp/" ;
         array_map('unlink', glob( "$fullPath*.pdf"));
 
